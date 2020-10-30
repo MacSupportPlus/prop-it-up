a93c70ec216c9ca1e43ed35a684cdce8db976808
Jason Mcelvain <macsupport423@Jasons-MacBook-Air.local>
2020-10-30 03:19:56 -0500

    components, components

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -2,14 +2,26 @@ import React, {Component} from 'react';
 
 export default class PersonCard extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            newAge : this.props.person.age
+            
+        }
+    }
+
     
-    render() {
+    render(){
         return (
             <div class="card">
                 <div class="card-body">
                     <h4 class="card-title">{this.props.person.firstname}, {this.props.person.lastname}</h4>
-                    <p class="card-text">age: {this.props.person.age}</p>
+                    <p class="card-text">age: {this.state.newAge}</p>
                     <p class="card-text">Hair Color: {this.props.person.haircolor}</p>
+                    <button 
+                    className="btn btn-success"
+                    onClick={() => this.setState({newAge: this.state.newAge+1})}
+                    >Birthday Button for {this.props.person.firstname} </button>
                     
 
                 </div>
@@ -18,6 +30,7 @@ export default class PersonCard extends Component {
           
         )
     }
+
 
 }
 
